855da6505b132a0098d3376676ea594e7279d8e2
Cristian Espinoza <ceegarner@gmail.com>
2021-08-03 16:00:04 -0600

    added configurations

--- src/MerkleDistributor.sol
+++ src/MerkleDistributor.sol
@@ -1,4 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+
 pragma solidity ^0.8.6;
 
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
 contract MerkleDistributor {
+   address public immutable override token;
+   bytes32 public immutable override merkleRoot;
+   // This is a packed array of booleans.
+   mapping(uint256 => uint256) private claimedBitMap;
+
+   constructor(address token_, bytes32 merkleRoot_) public {
+      token = token_;
+      merkleRoot = merkleRoot_;
+   }
 }
---
+++ src/tests/MerkleDistributor.t.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.6;
+
+import ".././MerkleDistributor.sol";
+import "../../lib/ds-test/src/test.sol";
+
+contract MerkleDistributorTest is DSTest {
+   MerkleDistributor distributor;
+   bytes32 merkleRoot = "";
+   bytes32 token = "0x321c2fe4446c7c963dc41dd58879af648838f98d";
+
+   function setUp() public {
+      distributor = new MerkleDistributor(merkleRoot, token);
+   }
+
+   function invariant_values() {
+      assertEq(distributor.merkleRoot(), merkleRoot);
+      assertEq(distributor.token(), token);
+   }
+
+   function test_claim() public {}
+
+   function testFail_basic_sanity() public {
+      assertTrue(false);
+   }
+
+   function test_basic_sanity() public {
+      assertTrue(true);
+   }
+}
